0c282f8685955e4e4cfb8c3416434dc3fa507228
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 <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>
2023-04-17 21:06:15 -0400

    latest

--- fbsde/fbsde_cnn.py
+++ fbsde/fbsde_cnn.py
@@ -1,276 +1,178 @@
+# simulation based on section 5 of https://www.sciencedirect.com/science/article/pii/S0005109817304740
+
 import torch
 from torch import nn
 from torch.autograd import grad
 from torch.nn import functional as F
 from torch.utils.data import DataLoader, random_split
-#from torchvision import datasets, transforms
 import numpy as np
-
-#from torch.distributions import MultivariateNormal, Uniform, TransformedDistribution, SigmoidTransform
 import pytorch_lightning as pl
-#from pytorch_lightning.trainer.supporters import CombinedLoader
-
-#from torchvision.utils import make_grid
-#import random
 import matplotlib
-matplotlib.use('agg')
+#matplotlib.use('agg')
 import matplotlib.pyplot as plt
-#from sklearn.datasets import make_swiss_roll
-#from sklearn.datasets import make_moons
 import sys
 from random import randint
 import seaborn as sns 
 import pandas as pd
-from torchqrnn import QRNN
 import time
 
-D_IDX = -1
-T_IDX = -2
-N_IDX =  0
+def b(t,x):
+    return torch.sin(x)
 
-def drift(x, coef):
-    x = x.unsqueeze(-1)
-    x0 = x ** 0
-    x1 = x ** 1
-    x2 = x ** 2
-    vals = torch.cat((x0,x1,x2),axis=-1)
-    return (coef * vals).sum(-1)
+def sigma(t,x):
+    return torch.cos(torch.Tensor([t]))
 
-def diffusion(x,t):
-    return 1
-def initial(x):
-    return torch.sin(6*np.pi*x).sum(-1)
-def initial_val(x):
-    return torch.sin(1*np.pi*x)
-def r_value():
-    return 1
+def sigma_batch(t,x):
+    return torch.cos(t)
 
-class Swish(nn.Module):
-    def __init__(self):
-        super().__init__()
+def g(x):
+    return torch.cos(x)
 
-    def forward(self, x): 
-        return torch.sigmoid(x)*x
+def h(t,x,y,z):
+    return torch.cos(t+x+y+z)
 
-class MLP(nn.Module):
-    def __init__(self, input_dim, hidden_dim, output_dim):
-        super().__init__()
+def divergence(y, x):
+        div = 0.
+        for i in range(y.shape[-1]):
+            div += torch.autograd.grad(y[..., i], x, torch.ones_like(y[..., i]), create_graph=True)[0][..., i:i+1]
+        return div
 
-        self.act = Swish()
-        self.input_fc = nn.Linear(input_dim, hidden_dim)
-        self.hidden_fc = nn.Linear(hidden_dim, hidden_dim)
-        self.output_fc = nn.Linear(hidden_dim, output_dim)
+def trace_grad(mu, Wt, N=10):
 
-    def forward(self, x):
-        batch_size = x.shape[0]
-        x = x.view(batch_size, -1)
-        h_1 = F.tanh(self.input_fc(x))
-        h_2 = F.tanh(self.hidden_fc(h_1))
-        y_pred = self.output_fc(h_2)
-        return y_pred
+    # Hutchinson's trace trick
 
-class CNN(nn.Module):
-	def __init__(self, input_size, hidden_size, num_layers, num_outputs):
-		super(CNN, self).__init__()
-		#self.num_layers = num_layers
-		#self.hidden_size = hidden_size
-		self.conv1 = nn.Conv1d(in_channels=input_size, out_channels=hidden_size, kernel_size=1, padding=0)
-		self.act1 = nn.Softplus()
-		self.conv2 = nn.Conv1d(in_channels=hidden_size, out_channels=hidden_size, kernel_size=1, padding=0)
-		self.act2 = nn.Softplus()
-		self.conv3 = nn.Conv1d(in_channels=hidden_size, out_channels=num_outputs, kernel_size=1, padding=0)
-		self.act3 = nn.Softplus()
-	
-	def forward(self, x):
-		out = self.conv1(x)
-		out = self.act1(out)
-		out = self.conv2(out)
-		out = self.act2(out)
-		out = self.conv3(out)
-		#out = self.act3(out)
-		return out
+    dmu = 0
+    for _ in range(N):
+        #mu  = self.mu(Wt)
+        v = torch.randn_like(mu) 
+        dmu += (v * grad(mu, Wt, grad_outputs=v, create_graph=True)[0]).sum(-1).sum(-1) / N
 
-
-class FKModule(pl.LightningModule):
-    def __init__(self, N = 2000, lr = 1e-3, X = 1., T = 0.1, dim = 2, batch_size = 100, num_time = 100, n_batch_val=100):
-        super().__init__()
-        # define normalizing flow to model the conditional distribution rho(x,t)=p(y|x,t)
-        self.num_time = num_time
-        self.T = T
-        self.t = torch.linspace(0,1,steps=self.num_time)* self.T
-        self.dim = dim
-        self.batch_size = batch_size
-        # input size is dimension of brownian motion x 2, since the input to the RNN block is W_s^x and dW_s^x
-        input_size = self.dim * 2 + 1
-        # hidden_size is dimension of the RNN output
-        hidden_size = 80
-        # num_layers is the number of RNN blocks
-        num_layers = 3
-        # num_outputs is the number of ln(rho(x,t))
-        num_outputs = self.dim
-        self.sequence = CNN(input_size, hidden_size, num_layers, num_outputs)
-        #self.sequence.load_state_dict(torch.load('/scratch/xx84/girsanov/pde_rnn/rnn_prior.pt'))
-
-        # define the learning rate
-        self.lr = lr
-                
-        # define number of paths used and grid of PDE
-        self.N = N
-        self.dt = self.t[1]-self.t[0] # define time step
-
-        # define the brwonian motion starting at zero
-        self.dB = np.sqrt(self.dt.item()) * np.random.randn(self.t.shape[0], self.N, self.batch_size, self.dim)
-        self.dB[0,:,:,:] = 0 
-        self.B0 = self.dB.copy()
-        self.B0 = torch.Tensor(self.B0.cumsum(0)).to(device)
-        self.dB = torch.Tensor(self.dB).to(device)
-        
-        self.metrics = torch.zeros((50,n_batch_val))
-        self.gir_metrics = torch.zeros((50,n_batch_val))
-        self.comp_time = torch.zeros((50,n_batch_val))
-        self.gir_comp_time = torch.zeros((50,n_batch_val))
-        self.rnn_comp_time = torch.zeros((50,n_batch_val))
-        self.epochs = torch.linspace(0,49,50)
-        
-        self.relu = torch.nn.Softplus()
-
-        self.coef_train = torch.rand(10,1,1,3).to(device)
-        
-        self.relu = torch.nn.Softplus()
-
-    def loss(self, xt, coef):
-        xs = xt[:,:-1]
-        ts = xt[:,-1]
-        coef = coef
-        Bx = (xs.unsqueeze(0).unsqueeze(0)+self.B0)
-        p0Bx = initial(Bx)
-        # calculate values using euler-maruyama
-        start = time.time()
-        x = torch.zeros(self.num_time, self.N, batch_size, self.dim).to(device)
-        x[0,:,:,:] = xs.squeeze()
-        for i in range(self.num_time-1):
-            x[i+1,:,:,:] = x[i,:,:,:] + drift(x[i,:,:,:], coef).squeeze() * self.dt + self.dB[i,:,:,:]
-        p0mux = initial(x)
-        u_em = p0mux.mean(1)
-        end = time.time()
-        time_em = (end - start)
-        # calculate values using girsanov
-        start = time.time()
-        muBx = drift(Bx, coef)
-        expmart = torch.exp((torch.cumsum(muBx*self.dB,dim=0) - 0.5 * torch.cumsum((muBx ** 2) * self.dt,dim=0)).sum(-1))
-        u_gir = (p0Bx*expmart).mean(1)
-        end = time.time()
-        time_gir = (end - start)
-        # calculate values using RNN
-        start = time.time()
-        input = torch.zeros(self.num_time, self.N, self.batch_size, self.dim * 2 + 1).to(device)
-        input[:muBx.shape[0],:,:,:] = torch.cat((muBx,self.dB,self.dt*torch.ones(self.dB.shape[0],self.dB.shape[1],self.dB.shape[2],1).to(device)),dim=-1)
-        input_reshaped = input.reshape(input.shape[1]*input.shape[2], input.shape[3], input.shape[0])
-        rnn_expmart = self.relu(self.sequence(input_reshaped).sum(-2)).reshape(p0Bx.shape)
-        u_rnn = (p0Bx*rnn_expmart).mean(1)
-        end = time.time()
-        time_rnn = (end - start)
-        return u_em, u_gir, u_rnn, time_em, time_gir, time_rnn
-
-    def training_step(self, batch, batch_idx):
-        # REQUIRED
-        xt = batch.to(device)
-        u_em, u_gir, u_rnn, time_em, time_gir, time_rnn = self.loss(xt, coef=torch.rand(1,1,1,3).to(device))
-        loss = F.l1_loss(u_rnn, u_gir)#/(torch.abs(u_gir).mean())
-        #tensorboard_logs = {'train_loss': loss_prior}
-        self.log('train_loss', loss)
-        #print(loss_total)
-        return {'loss': loss}
-        
-        
-    def validation_step(self, batch, batch_idx):
-        xt = batch.to(device)
-        u_em, u_gir, u_rnn, time_em, time_gir, time_rnn = self.loss(xt, coef=torch.rand(1,1,1,3).to(device))
-        loss = F.mse_loss(u_rnn,u_em,reduction='mean')/(torch.abs(u_em).mean())
-        loss_g = F.mse_loss(u_gir,u_em,reduction='mean')/(torch.abs(u_em).mean())
-        print('Validation: {:.4f}, {:.4f}'.format(loss, loss_g))
-        self.log('val_loss', loss)
-        if not loss.isnan():
-            self.metrics[self.current_epoch, batch_idx] = loss.item()
-            self.gir_metrics[self.current_epoch, batch_idx] = loss_g.item()
-            self.comp_time[self.current_epoch, batch_idx] = time_em
-            self.gir_comp_time[self.current_epoch, batch_idx] = time_gir
-            self.rnn_comp_time[self.current_epoch, batch_idx] = time_rnn
-        ep = torch.arange(self.metrics.shape[0])
-        plt.plot(ep, self.metrics.mean(-1), label='CNN')
-        plt.fill_between(ep, self.metrics.mean(-1) - self.metrics.std(-1), self.metrics.mean(-1) + self.metrics.std(-1), alpha=0.2)
-        plt.plot(ep, self.gir_metrics.mean(-1), label='Direct Girsanov')
-        plt.fill_between(ep, self.gir_metrics.mean(-1) - self.gir_metrics.std(-1), self.gir_metrics.mean(-1) + self.gir_metrics.std(-1), alpha=0.2)
-        plt.ylabel('Relative Error')
-        plt.xlabel('Epochs')
-        plt.legend()
-        plt.savefig('muBx_2d_cnn_gir.png')
-        plt.clf()
-        plt.plot(ep, self.metrics.mean(-1), label='CNN')
-        plt.fill_between(ep, self.metrics.mean(-1) - self.metrics.std(-1), self.metrics.mean(-1) + self.metrics.std(-1), alpha=0.2)
-        plt.ylabel('Relative Error')
-        plt.xlabel('Epochs')
-        plt.legend()
-        plt.savefig('muBx_2d_cnn.png')
-        plt.clf()
-        plt.plot(ep, self.comp_time.mean(-1), label='EM')
-        plt.fill_between(ep, self.comp_time.mean(-1) - self.comp_time.std(-1), self.comp_time.mean(-1) + self.comp_time.std(-1), alpha=0.2)
-        plt.plot(ep, self.gir_comp_time.mean(-1), label='Direct Girsanov')
-        plt.fill_between(ep, self.gir_comp_time.mean(-1) - self.gir_comp_time.std(-1), self.gir_comp_time.mean(-1) + self.gir_comp_time.std(-1), alpha=0.2)
-        plt.plot(ep, self.rnn_comp_time.mean(-1), label='CNN')
-        plt.fill_between(ep, self.rnn_comp_time.mean(-1) - self.rnn_comp_time.std(-1), self.rnn_comp_time.mean(-1) + self.rnn_comp_time.std(-1), alpha=0.2)
-        plt.ylabel('Computation Time')
-        plt.xlabel('Epochs')
-        plt.legend()
-        plt.savefig('comp_time_rnn.png')
-        plt.clf()
-        torch.save(self.sequence.state_dict(), '/scratch/xx84/girsanov/pde_rnn/cnn_10d_girloss.pt')
-        return #{'loss': loss_total}
-
-    def configure_optimizers(self):
-        optimizer = torch.optim.AdamW(self.parameters(), lr=self.lr)
-        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer)
-        return {"optimizer": optimizer, "lr_scheduler": scheduler, "monitor": "train_loss"}
-
-
+    return dmu
 
 if __name__ == '__main__':
     pl.seed_everything(1234)
     print(sys.executable)
-    #dataset = MNIST(os.getcwd(), train=True, download=True, transform=transforms.ToTensor())
-    #mnist_test = MNIST(os.getcwd(), train=False, download=True, transform=transforms.ToTensor())
-    #mnist_train, mnist_val = random_split(dataset, [55000,5000])
-    device = torch.device("cuda:0")
+
     
-    X = 0.5
+    # EM calculation of FBSDE
+    x_num = 60
+    dim = 1
+    N = 200
+    
+    x0 = torch.linspace(0.,0.6, x_num).unsqueeze(1)#torch.rand(x_num, dim)
+    t0 = torch.Tensor([0.])
     T = 0.1
-    num_time = 40
-    dim = 10
-    num_samples = 12000
-    batch_size = 60
-    N = 4000
-    xs = torch.rand(num_samples,dim) * X
-    ts = torch.rand(num_samples,1) * T
-    dataset = torch.cat((xs,ts),dim=1)
-    data_train = dataset[:num_samples// 2,:]
-    data_val = dataset[num_samples //2 :,:]
+    num_steps = 40
+    t = torch.linspace(t0.item(), T, steps=num_steps)
+    dt = t[1]-t[0]
+    dB = np.sqrt(dt.item()) * torch.randn(N, dim, num_steps)
+    dB = dB.unsqueeze(0).repeat(x_num,1,1,1)
     
-    train_kwargs = {'batch_size': batch_size,
-            'shuffle': True,
-            'num_workers': 1}
-
-    test_kwargs = {'batch_size': batch_size,
-            'shuffle': False,
-            'num_workers': 1}
-
-    n_batch_val = int(num_samples // 2 / batch_size)
-
-    train_loader = torch.utils.data.DataLoader(data_train,**train_kwargs)
-    val_loader = torch.utils.data.DataLoader(data_val, **test_kwargs)
-
-    model = FKModule(X=X, T=T, batch_size=batch_size, dim=dim, num_time=num_time, N=N, n_batch_val=n_batch_val)
-    trainer = pl.Trainer(max_epochs=50, gpus=1, check_val_every_n_epoch=1)
-    trainer.fit(model, train_loader, val_loader)
+    xi= x0.unsqueeze(1).repeat(1,N,1)
+    xi = xi.unsqueeze(-1)
+    xi.requires_grad = True
+    for i in range(0,num_steps-1):
+        x_current = xi[:,:,:,i] + b(t[i], xi[:,:,:,i]) * dt + sigma(t[i], xi[:,:,:,i]) * dB[:,:,:,i]
+        xi = torch.cat((xi, x_current.unsqueeze(-1)),dim=-1)
     
-    print(trainer.logged_metrics['val_loss'])
-    print(trainer.logged_metrics['train_loss'])
+    xT = xi[:,:,:,-1]
+    yT = g(xT)
+    yi = torch.zeros_like(xi)
+    yi[:,:,:,-1] = yT
+    vi = yT.mean(1)
+    z_current = sigma(T,xT) * torch.autograd.grad(outputs=vi,inputs=xT,grad_outputs=torch.ones_like(vi))[0]
+    for i in reversed(range(1,num_steps)):
+        x_current = xi[:,:,:,i]
+        t_current = t[i]
+        yi[:,:,:,i-1] = yi[:,:,:,i] + h(t_current,x_current,yi[:,:,:,i],z_current) * dt
+        vi = yi[:,:,:,i-1].mean(1)
+        z_current = sigma(t_current,x_current) * torch.autograd.grad(outputs=vi,inputs=x_current,grad_outputs=torch.ones_like(vi))[0]
+        
+    vis = yi.mean(1)
+    
+    # change of PDE calculation of FBSDE
+    t = torch.linspace(t0.item(), T, steps=num_steps)
+    dt = t[1]-t[0]
+    dB = np.sqrt(dt.item()) * torch.randn(N, dim, num_steps)
+    dB = dB.unsqueeze(0).repeat(x_num,1,1,1)
+    
+    #xi= x0.unsqueeze(1).repeat(1,N,1)
+    #xi = xi.unsqueeze(-1)
+    #xi.requires_grad = True
+    #for i in range(0,num_steps-1):
+    #    x_current = xi[:,:,:,i] + sigma(t[i], xi[:,:,:,i]) * dB[:,:,:,i]
+    #    xi = torch.cat((xi, x_current.unsqueeze(-1)),dim=-1)
+        
+    
+    xi = torch.cumsum(dB * sigma_batch(t,x0).unsqueeze(0).unsqueeze(0).unsqueeze(0),dim=-1) + x0.unsqueeze(-1).unsqueeze(-1).repeat(1,N,1,num_steps)
+    xi.requires_grad = True
+    
+    xT = xi[:,:,:,-1]
+    yT = g(xT)
+    yi = torch.zeros_like(xi)
+    yi[:,:,:,-1] = yT
+    vi = yT.mean(1)
+    z_current = sigma(T,xT) * torch.autograd.grad(outputs=vi,inputs=xT,grad_outputs=torch.ones_like(vi))[0]
+    for i in reversed(range(1,num_steps)):
+        x_current = xi[:,:,:,i]
+        t_current = t[i]
+        yi[:,:,:,i-1] = yi[:,:,:,i] + (h(t_current,x_current,yi[:,:,:,i],z_current) + z_current * (1/sigma(t_current, x_current)) * b(t_current, x_current)) * dt
+        vi = yi[:,:,:,i-1].mean(1)
+        z_current = sigma(t_current,x_current) * torch.autograd.grad(outputs=vi,inputs=x_current,grad_outputs=torch.ones_like(vi))[0]
+        
+    vis_cov = yi.mean(1)
+    
+    # Girsanov calculation of FBSDE
+    t = torch.linspace(t0.item(), T, steps=num_steps)
+    dt = t[1]-t[0]
+    dB = np.sqrt(dt.item()) * torch.randn(N, dim, num_steps)
+    dB = dB.unsqueeze(0).repeat(x_num,1,1,1)
+    
+    #xi= x0.unsqueeze(1).repeat(1,N,1)
+    #xi = xi.unsqueeze(-1)
+    #xi.requires_grad = True
+    #for i in range(0,num_steps-1):
+    #    x_current = xi[:,:,:,i] + sigma(t[i], xi[:,:,:,i]) * dB[:,:,:,i]
+    #    xi = torch.cat((xi, x_current.unsqueeze(-1)),dim=-1)
+        
+    sigmas = sigma_batch(t,x0)
+    xi = torch.cumsum(dB * sigmas.unsqueeze(0).unsqueeze(0).unsqueeze(0),dim=-1) + x0.unsqueeze(-1).unsqueeze(-1).repeat(1,N,1,num_steps)
+    xi.requires_grad = True
+    
+    sigmas = sigmas.unsqueeze(0).unsqueeze(0).unsqueeze(0).repeat(x_num, N, dim, 1)
+    mu = b(t, xi)/sigmas
+    
+    mart = torch.cumsum(mu * dB, dim=-1) - 0.5 * torch.cumsum(mu ** 2, dim=-1) * dt
+    expmart = torch.exp(mart)
+    
+    xT = xi[:,:,:,-1]
+    yT = g(xT)
+    yi = torch.zeros_like(xi)
+    yi[:,:,:,-1] = yT * expmart[:,:,:,-1]
+    vi = yT.mean(1)
+    z_current = sigma(T,xT) * torch.autograd.grad(outputs=vi,inputs=xT,grad_outputs=torch.ones_like(vi))[0]
+    for i in reversed(range(1,num_steps)):
+        x_current = xi[:,:,:,i]
+        t_current = t[i]
+        yi[:,:,:,i-1] = yi[:,:,:,i] + h(t_current,x_current,yi[:,:,:,i],z_current) * dt * expmart[:,:,:,i-1]
+        vi = yi[:,:,:,i-1].mean(1)
+        z_current = sigma(t_current,x_current) * torch.autograd.grad(outputs=vi,inputs=x_current,grad_outputs=torch.ones_like(vi))[0]
+        
+    vis_gir = yi.mean(1)
+    
+    plt.subplot(2,2,1)
+    plt.imshow(vis.squeeze().detach().numpy())
+    plt.colorbar()
+    
+    plt.subplot(2,2,2)
+    plt.imshow(vis_gir.squeeze().detach().numpy())
+    plt.colorbar()
+    
+    plt.subplot(2,2,3)
+    plt.imshow(torch.abs(vis-vis_cov).squeeze().detach().numpy())
+    plt.colorbar()
+    
+    plt.subplot(2,2,4)
+    plt.imshow(torch.abs(vis-vis_gir).squeeze().detach().numpy())
+    plt.colorbar()
+    plt.show()
