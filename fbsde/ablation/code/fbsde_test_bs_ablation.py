bd269957e5e5f114bf44d5410c44e3c622f67265
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 <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>
2023-05-09 22:54:47 -0400

    latest

--- fbsde/ablation/code/fbsde_test_bs_ablation.py
+++ fbsde/ablation/code/fbsde_test_bs_ablation.py
@@ -370,7 +370,7 @@ if __name__ == '__main__':
     em_time_max = []
     em_time_var = []
     
-    for dim in range(1,12):
+    for dim in range(1,15):
         i = 10
         m=100
         p=15
